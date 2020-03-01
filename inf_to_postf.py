ea84976c0d29b0a4b87c5edcf61f97cda9c23e48
ixinit <asrtemmashenkoff20158@gmail.com>
2020-03-01 13:40:01 +0300

    Inital commit

---
+++ inf_to_postf.py
@@ -0,0 +1,38 @@
+from pythonds.basic.stack import Stack
+print("Введите инфиксое выражение с пробелом после каждго символа")
+print("Пример: ( A + B * C ) - D")
+form = input()
+def infixToPostfix(infixexpr):
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    opStack = Stack()
+    postfixList = []
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and \
+               (prec[opStack.peek()] >= prec[token]):
+                  postfixList.append(opStack.pop())
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+    return " ".join(postfixList)
+
+#print(infixToPostfix("A * B + C * D"))
+#print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+print(infixToPostfix(form))
